cea881bd038f97841376c3cabf7356cc4b409721
EnesAydogan14 <9027461@student.zadkine.nl>
2025-10-15 12:53:16 +0200

    opdr 1.4

---
+++ opdracht-01-books/src/components/BookCounter.jsx
@@ -0,0 +1,9 @@
+const BookCounter = ({ aantal }) => {
+    return (
+        <div>
+            <h2>Aantal keer gelezen: {aantal}</h2>
+        </div>
+    );
+}
+ 
+export default BookCounter;
--- opdracht-01-books/src/components/BookList.jsx
+++ opdracht-01-books/src/components/BookList.jsx
@@ -1,6 +1,7 @@
 import Book from "./book";
 import {useState} from 'react';
 import data from "../book-data.js/";
+import BookCounter from "./BookCounter";
 
 const BookList = () => {
     const [books, setBooks] = useState(data);
@@ -9,6 +10,7 @@ const BookList = () => {
     return (
         <section className="book-list">
             <h2>Book List</h2>
+            <BookCounter aantal={books.length} />
             {books.map((book) => (
         <Book 
           key={book.id}
