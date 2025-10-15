40c525637dd77afb9215908d6d708595bf1ecb30
EnesAydogan14 <9027461@student.zadkine.nl>
2025-10-15 12:50:03 +0200

    opdr 1.3

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,9 +1,17 @@
+import {useState} from "react";
+
 const Book = ({ title, description, images }) => {
+     const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+     function verhoogTeller() {
+         setAantalKeerGelezen(aantalKeerGelezen + 1);
+     }
     return (  
     <section className="book-container">
         <h2>{title}</h2>
         <p>{description}</p>
         <img className="book-image" src={images} alt={title} />
+        <button onClick={verhoogTeller}>Verhoog teller</button>
+        <p>Aantal keer gelezen: {aantalKeerGelezen}</p>
         </section>
     );
 }
