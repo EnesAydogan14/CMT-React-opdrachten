8dfb653d78d79cb3196bf2bd75dae3b82d22ff9b
EnesAydogan14 <9027461@student.zadkine.nl>
2025-12-03 15:20:47 +0100

    verbetering opdr 11.2

--- opdracht-01-books/src/book-data.js
+++ opdracht-01-books/src/book-data.js
@@ -1,17 +1,20 @@
 const data = [
-     { id: 1, title: "Power", 
+     { id: 1, title: "Harry Potter", 
       author: "K.K. Rowlings",
-      image: './images/book-1.png' 
+      image: './images/book-1.png', 
+      category:"Sciencefiction"
      },
     { 
-     id: 2, title: "Psychology of Money", 
+     id: 2, title: "Transavia", 
      author: "Morgan Housel", 
-     image: "./images/book-2.png" 
+     image: "./images/book-2.png",
+     category: "Fantasy",
      },
      { 
-     id: 3, title: "Surrounded by Idiots",
+     id: 3, title: "The Hunger Games",
      author: "Suzanne Collins", 
-     image: "./images/book-3.png" 
+     image: "./images/book-3.png", 
+     category: "Thriller"
      }
 ]
 
