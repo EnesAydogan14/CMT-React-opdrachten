66ff29214d9578f19b56a6c1bd6ee38c2235fa6f
EnesAydogan14 <9027461@student.zadkine.nl>
2025-12-17 11:46:06 +0100

    opdr 13.1

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 const Book = ({ title, description, images, category }) => {
   const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
+  const [liked, setLiked] = useState(false); 
 
   const verhoogTeller = () => {
     setAantalKeerGelezen((prev) => prev + 1);
+  };
+
+  const toggleLike = () => {
+    setLiked(!liked);
   };
 
   return (
@@ -12,10 +17,19 @@ const Book = ({ title, description, images, category }) => {
       <h2>{title}</h2>
       <p>{description}</p>
 
-      
       <p><strong>Categorie:</strong> {category}</p>
 
       <img className="book-image" src={images} alt={title} />
+
+      <div className="favorite-section">
+        <button onClick={toggleLike}>
+          {liked ? "❤️" : "🤍"}
+        </button>
+
+        {liked && (
+          <p className="favorited-text">Toegevoegd aan favorieten</p>
+        )}
+      </div>
 
       <button onClick={verhoogTeller}>Verhoog teller</button>
       <p>Aantal keer gelezen: {aantalKeerGelezen}</p>
